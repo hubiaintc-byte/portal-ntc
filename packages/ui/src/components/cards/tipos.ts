f919001150af8ecfae2965dbbe4eea03f75a0dd3
Jotta11 <jgbgdondim@gmail.com>
2026-05-20 15:51:10 -0300

    feat(ui): re-export tipos compartilhados em cards/tipos.ts

---
+++ packages/ui/src/components/cards/tipos.ts
@@ -0,0 +1,8 @@
+export type { Area, ImagemRef, Modalidade } from "../heroes/tipos";
+export {
+  acentoPorArea,
+  rotuloArea,
+  rotuloModalidade,
+  formatarData,
+  formatarPeriodo,
+} from "../heroes/tipos";
